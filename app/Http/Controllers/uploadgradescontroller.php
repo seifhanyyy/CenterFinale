d6a17634ec6c180b6a80f08220120d27ef57fe70
Seifhr <47357615+Seifhr@users.noreply.github.com>
2020-06-12 23:13:33 +0200

    Update uploadgradescontroller.php

--- app/Http/Controllers/uploadgradescontroller.php
+++ app/Http/Controllers/uploadgradescontroller.php
@@ -11,11 +11,11 @@ class uploadgradescontroller extends Controller
     public function ShowAllStudents()//byreturn all students
     {
         $x = AUTH::user()->name;
-        $mar3ey=AUTH::user()->lastName;
-        $alpha=$x.' '.$mar3ey;
+        $y = AUTH::user()->lastName;
+        $name = $x." ".$y;
         $uploadgrade = DB::select("select * from users INNER JOIN studentandclasses on users.id = studentandclasses.studentId
-        INNER JOIN classes on studentandclasses.classId=classes.id where classes.teacher='$alpha'");
-        $statment2 = DB::select("select * from classes where teacher='$alpha'");
+        INNER JOIN classes on studentandclasses.classId=classes.id where classes.teacher='$name'");
+        //$statment2 = DB::select("select * from classes where teacher='$x'");
         //$uploadgrade = DB::select("select name,selected,courseid from $statment1 UNION $statment2");
         //$uploadgrade = DB::select("select * from users");
         return view('UploadGrade', ['uploadgrade' => $uploadgrade]);
@@ -26,8 +26,9 @@ class uploadgradescontroller extends Controller
         $week = $request->input('QuizWeek');
         $grade = $request->input('Grade');
         $sid = $request->input('id');
-        DB::update("update grades set quizweek=$week, grade=$grade where sid=$sid");
-        return view("/UploadGrade");
+        $cid = $request->input('courseId');
+        DB::update("update grades set quizweek=$week, grade=$grade where sid=$sid AND courseId=$cid");
+        return view("/Teacher");
     }
 
 
@@ -36,7 +37,8 @@ class uploadgradescontroller extends Controller
         $week = $request->input('QuizWeek');
         $grade = $request->input('Grade');
         $sid = $request->input('id');
-        $data = array('sid' => $sid, "quizweek" => $week, "grade" => $grade);
+        $cid = $request->input('courseId');
+        $data = array('sid' => $sid,"courseId"=>$cid, "quizweek" => $week, "grade" => $grade);
         DB::table('grades')->insert($data);
         return view("/Teacher");
     }
