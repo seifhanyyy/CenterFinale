3d4cde20d8855f242bc41c84bb179a81f8332635
Seifhr <47357615+Seifhr@users.noreply.github.com>
2020-06-12 23:29:56 +0200

    Update addclass.blade.php

--- resources/views/addclass.blade.php
+++ resources/views/addclass.blade.php
@@ -39,6 +39,7 @@
 
 <h2>Edit This Class</h2>
 <?php
+use Illuminate\Support\Facades\DB;
 $teachers = DB::select("Select * from users where type = '2'");
 $selected_val = '';
 
@@ -62,7 +63,7 @@ $selected_val = '';
     echo "<br>";
 
     echo "<label for='subject'>Subject:</label><br>"; //Subject
-    echo "<select name='day'>";
+    echo "<select name='subject'>";
       echo"<option value='English'>English</option>";
       echo"<option value='Math'>Math</option>";
       echo"<option value='Arabic'>Arabic</option>";
@@ -79,19 +80,94 @@ $selected_val = '';
     echo "<br>";
 
     echo "<label for='starts'>Starts at:</label><br>"; //Starts
-    echo "<input type='text' id='starts' name='starts' value=''><br><br>";
-
+    echo "<select name='starts'>";
+    echo"<option value='1:00'>1:00</option>";
+    echo"<option value='1:30'>1:30</option>";
+    echo"<option value='2:00'>2:00</option>";
+    echo"<option value='2:30'>2:30</option>";
+    echo"<option value='3:00'>3:00</option>";
+    echo"<option value='3:30'>3:30</option>";
+    echo"<option value='4:00'>4:00</option>";
+    echo"<option value='4:30'>4:30</option>";
+    echo"<option value='5:00'>5:00</option>";
+    echo"<option value='5:30'>5:30</option>";
+    echo"<option value='6:00'>6:00</option>";
+    echo"<option value='6:30'>6:30</option>";
+    echo"<option value='7:00'>7:00</option>";
+    echo"<option value='7:30'>7:30</option>";
+    echo"<option value='8:00'>8:00</option>";
+    echo"<option value='8:30'>8:30</option>";
+    echo"<option value='9:00'>9:00</option>";
+    echo"<option value='9:30'>9:30</option>";
+    echo"<option value='10:00'>10:00</option>";
+    echo"<option value='10:30'>10:30</option>";
+    echo"<option value='11:00'>11:00</option>";
+    echo"<option value='11:30'>11:30</option>";
+    echo"<option value='12:00'>12:00</option>";
+    echo"<option value='12:30'>12:30</option>";
+  echo"</select>";
+  echo"<br>";
     echo "<label for='ends'>Ends at:</label><br>"; //Ends
-    echo "<input type='text' id='ends' name='ends' value=''><br><br>";
+    echo "<select name='ends'>";
+    echo"<option value='1:00'>1:00</option>";
+    echo"<option value='1:30'>1:30</option>";
+    echo"<option value='2:00'>2:00</option>";
+    echo"<option value='2:30'>2:30</option>";
+    echo"<option value='3:00'>3:00</option>";
+    echo"<option value='3:30'>3:30</option>";
+    echo"<option value='4:00'>4:00</option>";
+    echo"<option value='4:30'>4:30</option>";
+    echo"<option value='5:00'>5:00</option>";
+    echo"<option value='5:30'>5:30</option>";
+    echo"<option value='6:00'>6:00</option>";
+    echo"<option value='6:30'>6:30</option>";
+    echo"<option value='7:00'>7:00</option>";
+    echo"<option value='7:30'>7:30</option>";
+    echo"<option value='8:00'>8:00</option>";
+    echo"<option value='8:30'>8:30</option>";
+    echo"<option value='9:00'>9:00</option>";
+    echo"<option value='9:30'>9:30</option>";
+    echo"<option value='10:00'>10:00</option>";
+    echo"<option value='10:30'>10:30</option>";
+    echo"<option value='11:00'>11:00</option>";
+    echo"<option value='11:30'>11:30</option>";
+    echo"<option value='12:00'>12:00</option>";
+    echo"<option value='12:30'>12:30</option>";
+  echo"</select>";
+  echo"<br>";
 
     echo "<label for='capacity'>Capacity:</label><br>"; //Capacity
-    echo "<input type='text' id='capacity' name='capacity' value=''><br><br>";
-
+    echo "<select name='capacity'>";
+    echo"<option value='1'>1</option>";
+    echo"<option value='2'>2</option>";
+    echo"<option value='3'>3</option>";
+    echo"<option value='4'>4</option>";
+    echo"<option value='5'>5</option>";
+    echo"<option value='6'>6</option>";
+    echo"<option value='7'>7</option>";
+    echo"<option value='8'>8</option>";
+    echo"<option value='9'>9</option>";
+    echo"<option value='10'>10</option>";
+    echo"<option value='11'>11</option>";
+    echo"<option value='12'>12</option>";
+    echo"<option value='13'>13</option>";
+    echo"<option value='14'>14</option>";
+    echo"<option value='15'>15</option>";
+  echo"</select>";
+  echo"<br>";
     echo "<label for='year'>Educational Year:</label><br>"; //Year
-    echo "<input type='text' id='year' name='year' value=''><br><br>";
-
+    echo "<select name='year'>";
+    echo"<option value='1'>1</option>";
+    echo"<option value='2'>2</option>";
+    echo"<option value='3'>3</option>";
+  echo"</select>";
+  echo"<br>";
     echo "<label for='gender'>Gender:</label><br>"; //Gender
-    echo "<input type='text' id='gender' name='gender' value=''><br><br>";
+    echo "<select name='day'>";
+    echo"<option value='male'>male</option>";
+    echo"<option value='female'>female</option>";
+  echo"</select>";
+  echo"<br>";
 
     echo "<input type='submit' value='Submit'>";
     echo "</form>";
