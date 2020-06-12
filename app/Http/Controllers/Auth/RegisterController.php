ffc819fa42c3237d354972a74f17b178a60f17d2
Seifhr <47357615+Seifhr@users.noreply.github.com>
2020-06-13 00:19:15 +0200

    Update RegisterController.php

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -74,7 +74,8 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
+            'name' => ['required', 'alpha', 'max:10'],
+            'LastName' => ['required', 'alpha', 'max:10'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
             'phonenum' => 'required|regex:/(01)[0-9]{9}/',
@@ -87,7 +88,8 @@ class RegisterController extends Controller
     protected function validator2(array $data2)
     {
         return Validator::make($data2, [
-            'name' => ['required', 'string', 'max:255'],
+            'name' => ['required', 'alpha', 'max:10'],
+            'LastName' => ['required', 'alpha', 'max:10'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
             'phonenum' => 'required|regex:/(01)[0-9]{9}/',
